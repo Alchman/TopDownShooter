3ee94bcf393239d9aef6faefaba9a6d68cf8e5e2
Default Profile <Default Profile>
2020-04-16 21:15:37 +0300

    Animations

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,12 +7,13 @@ public class PlayerMovement : MonoBehaviour
     public float speed;
 
     Rigidbody2D rb;
-
+    Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
@@ -28,6 +29,8 @@ public class PlayerMovement : MonoBehaviour
         float inputY = Input.GetAxis("Vertical");
 
         rb.velocity = new Vector2(inputX, inputY) * speed;
+        anim.SetFloat("Speed", rb.velocity.magnitude);
+
 
         //move with updating transform directly
         //Vector3 newPosition = transform.position;
